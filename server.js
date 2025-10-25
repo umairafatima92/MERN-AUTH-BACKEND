07d9144004adfc9f19d31d80b4f460d0aa0ecbc9
umairafatima92 <umairafatima897@gmail.com>
2025-10-25 11:00:51 +0500

    updated server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import cors from "cors";
-
 import cookieParser from "cookie-parser";
 import connectDB from "./config/mongodb.js";
 import authRouter from "./routes/authRoutes.js";
@@ -10,23 +9,32 @@ import userRouter from "./routes/userRoutes.js";
 
 const app = express();
 
+
 const port = process.env.PORT || 8000;
 
 connectDB();
 
 const allowedOrigins = [
-     "http://localhost:5173"
+    "http://localhost:5173",
+    "https://mern-auth-frontend-gray.vercel.app" 
+     
 ];
 
+// Middleware
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: allowedOrigins, credentials: true }));
+app.use(cors({ 
+    origin: allowedOrigins, 
+    credentials: true 
+}));
 
+// Routes
 app.get("/", (req, res) => res.send("API working"));
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
-console.log(process.env.PORT);
 
 
-app.listen(port, () => console.log(`server started on PORT:${port}`));
- 
+console.log(`PORT: ${port}`);
+
+
+app.listen(port, () => console.log(`Server started on PORT: ${port}`));
